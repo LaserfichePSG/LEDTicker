0b5ca5f3d43bf899cf05538404e57911c6d19084
Josh Kimmel <josh.kimmel@laserfiche.com>
2017-02-16 12:51:44 -0800

    Adjusting config params.

--- Led_Config.py
+++ Led_Config.py
@@ -1,6 +1,6 @@
 class Led_Config:
     def __init__(self):
-            self.board_length = 300
+            self.board_length = 60
             self.board_height = 5
             self.strip_options = {"led_pin": 18, "led_frequency": 800000, "led_dma": 5, "led_invert": False, "led_brightness": 150}
             self.display_count = 5
