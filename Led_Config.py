5ed943d2a404e71c7b020f1f5006a570870ccfaa
Josh Kimmel <josh.kimmel@laserfiche.com>
2017-02-16 07:28:28 -0800

    Fixing syntax error in config file.

--- Led_Config.py
+++ Led_Config.py
@@ -1,7 +1,7 @@
 class Led_Config:
     def __init__(self):
-            self.board_length = 300,
-            self.board_height = 5,
-            self.strip_options = {"led_pin": 18, "led_frequency": 800000, "led_dma": 5, "led_invert": False, "led_brightness": 150},
-            self.display_count = 5,
+            self.board_length = 300
+            self.board_height = 5
+            self.strip_options = {"led_pin": 18, "led_frequency": 800000, "led_dma": 5, "led_invert": False, "led_brightness": 150}
+            self.display_count = 5
             self.scroll_buffer = 0.1
